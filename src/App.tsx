8a61efcfa30f8ecf21117dc741233681415f2b9f
jaysson18 <jahaka999@gmail.com>
2025-01-22 10:22:53 +0200

    routing tekoa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Upload from './views/Upload';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
     <>
-      <h1>My App</h1>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Profile" element={<Profile />}></Route>
+            <Route path="/Upload" element={<Upload />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/views/Single.tsx
@@ -0,0 +1,10 @@
+const Single = () => {
+  return (
+    <div>
+      <h1>Single View</h1>
+      <p>This is the Single view placeholder.</p>
+    </div>
+  );
+};
+
+export default Single;
